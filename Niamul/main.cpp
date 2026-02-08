711ce4cf1b3987efa2755f77f760a9fc950f407f
snipingTurtle <aulajhaula.niam03@gmail.com>
2026-02-09 00:09:36 +0600

    add oop

---
+++ Niamul/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class User
+{
+private:
+    int id;
+    string name;
+    string priviledges;
+
+public:
+    User() : id(0), name("NULL"), priviledges("NULL")
+    {
+    }
+
+    User(const int &user_id, const string &s, const string &priv) : id(user_id), name(s), priviledges(priv)
+    {
+    }
+
+    void Register()
+    {
+
+    }
+
+    void LogIn()
+    {
+
+    }
+
+    void LogOut()
+    {
+        
+    }
+};
+
+int main()
+{
+}
