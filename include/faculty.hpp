dc2943fe59b015f2f8e576364958c9c2d65c775c
Zahin Ahmad <zahin2784@example.com>
2026-03-27 23:04:03 +0600

    Add faculty and faculty_database classes, update CMakeLists, faculty database is inside Database folder

---
+++ include/faculty.hpp
@@ -0,0 +1,54 @@
+#ifndef FACULTY_H
+#define FACULTY_H
+#include <string>
+#include "person.hpp"
+using namespace std;
+class faculty : public Person{
+    private:
+        int id;
+        int roomNumber;
+        string hall;
+        string password;
+        string email;
+        int unreadMessages;
+    public:
+        faculty(){
+            unreadMessages = 0;
+        }
+        //int roomNumber, std::string name, string password, string email, int phoneNumber, std::string bloodGroup, int emergencyContact, int id, string hall
+        faculty(int id, string name, string email, int phoneNumber, int emergencyContact, int roomNumber, string bloodGroup, string hall, string password) : Person(name, phoneNumber, bloodGroup, emergencyContact){
+            this->roomNumber = roomNumber;
+            this->id = id;
+            this->hall = hall;
+            this->email = email;
+            this->password = password;
+            unreadMessages = 0;
+        }
+        // ================== GETTER DECLARATIONS ==================
+        int getId() const;
+        int getRoomNumber() const;
+        string getHall() const;
+        string getPassword() const;
+        string getEmail() const;
+        int getUnreadMessages() const;
+
+        // ================== SETTER DECLARATIONS ==================
+        void setId(int newId);
+        void setRoomNumber(int newRoomNumber);
+        void setHall(const string& newHall);
+        void setPassword(const string& newPassword);
+        void setEmail(const string& newEmail);
+        void setUnreadMessages(int newUnreadMessages);
+
+
+        void getCountOfUnreadMessages();
+        void readMessages();
+        void writeMessage();
+        void readReadMessagesFile();
+
+
+
+        //bool checkFirstTimeLogin();
+        //void firstTimeLogin();
+};
+#endif
---
+++ src/faculty.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "../include/faculty.hpp"
+#define nl '\n'
+using namespace std;
+
+// ================== GETTER IMPLEMENTATIONS ==================
+
+int faculty::getId() const { 
+    return id; 
+}
+
+int faculty::getRoomNumber() const { 
+    return roomNumber; 
+}
+
+string faculty::getHall() const { 
+    return hall; 
+}
+
+string faculty::getPassword() const { 
+    return password; 
+}
+
+string faculty::getEmail() const { 
+    return email; 
+}
+
+int faculty::getUnreadMessages() const { 
+    return unreadMessages; 
+}
+
+// ================== SETTER IMPLEMENTATIONS ==================
+
+void faculty::setId(int newId) { 
+    id = newId; 
+}
+
+void faculty::setRoomNumber(int newRoomNumber) { 
+    roomNumber = newRoomNumber; 
+}
+
+void faculty::setHall(const string& newHall) { 
+    hall = newHall; 
+}
+
+void faculty::setPassword(const string& newPassword) { 
+    password = newPassword; 
+}
+
+void faculty::setEmail(const string& newEmail) { 
+    email = newEmail; 
+}
+
+void faculty::setUnreadMessages(int newUnreadMessages) { 
+    unreadMessages = newUnreadMessages; 
+}
+void faculty::getCountOfUnreadMessages(){
+    string fileName = "../Messages/" + to_string(id) + "Unread.txt";
+    ifstream inFile(fileName);
+    string line;
+    while (getline(inFile, line)) {
+        unreadMessages++;
+    }
+    cout << unreadMessages << "messages" << "\n";
+}
+void faculty::readMessages(){
+    string fileName = "../Messages/" + to_string(id) + "Unread.txt";
+    ifstream inFile(fileName);
+    string fileName2 = "../Messages/" + to_string(id) + "Read.txt";
+    ofstream outFile;
+    outFile.open(fileName2, ios::app);
+    string line;
+    while (getline(inFile, line)) {
+        cout << line << "\n";
+        outFile << line << "\n";
+    }
+    inFile.close();
+    outFile.close();
+    outFile.open(fileName, ios::trunc);
+    if (outFile.is_open()) {
+        cout << "File '" << fileName << "' has been successfully cleared.\n";
+        outFile.close();
+    } else {
+        cerr << "Error: Could not open the file to clear it.\n";
+    }
+}
+void faculty::writeMessage(){
+    int destination;
+    cout << "Enter the id of recipient: ";
+    cin >> destination;
+    cin.ignore();
+    string fileName = "../Messages/" + to_string(destination) + "Unread.txt";
+    ofstream outFile;
+    outFile.open(fileName, ios::app);
+    string message;
+    cout << "Enter your message, press ~ and enter when done: " << "\n";
+    getline(cin, message, '~');
+    outFile << message << "\n";
+    cout << "Message Sent Successfully!" << "\n";
+    outFile.close();
+}
+void faculty::readReadMessagesFile(){
+    string fileName = "../Messages/" + to_string(id) + "Read.txt";
+    ifstream inFile(fileName);
+    string line;
+    while (getline(inFile, line)) {
+        cout << line << "\n";
+    }
+    inFile.close();
+}
+void Faculty(){
+    cout << "Faculty data" << nl;
+    //faculty f1(129, "Zahin", 159154, "O+", 39629, 123, "South Hall");
+    //f1.displayDetails();
+}
